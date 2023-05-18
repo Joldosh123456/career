10c92dd33a74acd2135dbdc276833d090f696134
Adiletkk <adilet_esirkepov02@mail.ru>
2023-05-18 17:29:19 +0500

    end2

--- src/components/BlockPage/BlockPage.jsx
+++ src/components/BlockPage/BlockPage.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import BlockPageLeft from "./BlockPageLeft/BlockPageLeft";
 import BlockPageRight from "./BlockPageRight/BlockPageRight";
 import css from './BlockPage.module.scss'
+import Header from "../HomePage/Header/Header";
+import Footer from "../HomePage/Footer/Footer";
 
 function BlockPage() {
   return (
     <div className={css.wrapper}>
+      <div>
+        <Header/>
+      </div>
       <div className="container">
         <div className={css.all_tegs}>
         <div className={css.left}>
@@ -15,6 +20,9 @@ function BlockPage() {
           <BlockPageRight />
         </div>
         </div>
+      </div>
+      <div>
+        <Footer/>
       </div>
     </div>
   );
